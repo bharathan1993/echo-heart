62504fcb68c846c2a3aaa67c2b70b98e3634dc0a
bharathan1993 <itzbharathan@gmail.com>
2024-11-28 13:37:53 +0530

    Vercel changes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,11 @@
 'use client'
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type UseThemeProps } from "next-themes"
+
+export interface ThemeProviderProps extends Omit<UseThemeProps, "children"> {
+  children: React.ReactNode;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
